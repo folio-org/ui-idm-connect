24fda030bc253f8f61d7e6e1fcc36544d5e47cc2
Viola Elsenhans <elsenhans@users.noreply.github.com>
2021-11-08 13:15:07 +0100

    UIIDM-10-add-tests-results-details first test

---
+++ src/components/view/ContractView.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useStripes } from '@folio/stripes/core';
+import { StripesContext } from '@folio/stripes-core/src/StripesContext';
+import { MemoryRouter } from 'react-router-dom';
+import renderWithIntl from '../../../test/jest/helpers/renderWithIntl';
+
+import ContractView from './ContractView';
+import contract from '../../../test/jest/fixtures/contract';
+
+const handlers = {
+  onClose: jest.fn,
+  onEdit: jest.fn,
+};
+
+const renderContract = (stripes) => {
+  return renderWithIntl(
+    <StripesContext.Provider value={stripes}>
+      <MemoryRouter>
+        <ContractView
+          handlers={handlers}
+          isLoading={false}
+          record={contract}
+          stripes={stripes}
+        />
+      </MemoryRouter>
+    </StripesContext.Provider>
+  );
+};
+
+describe('ContractView', () => {
+  let stripes;
+
+  beforeEach(() => {
+    stripes = useStripes();
+    renderContract(stripes);
+  });
+
+  it('should render contract', async () => {
+    await userEvent.click(screen.getByText('Personal data'));
+
+    expect(screen.getByText('Lastname')).toBeVisible();
+  });
+
+  it('should render accordions', () => {
+    expect(document.querySelector('#personalAccordion')).toBeInTheDocument();
+    expect(document.querySelector('#contractAccordion')).toBeInTheDocument();
+    expect(document.querySelector('#contactAccordion')).toBeInTheDocument();
+    expect(document.querySelector('#commentAccordion')).toBeInTheDocument();
+  });
+});
--- test/jest/__mock__/index.js
+++ test/jest/__mock__/index.js
@@ -1,8 +1,8 @@
+import './currencyData.mock';
 import './stripesConfig.mock';
 import './stripesCore.mock';
 import './stripes.mock';
 import './intl.mock';
 import './stripesIcon.mock';
-import './stripesComponents.mock';
 import './stripesSmartComponents.mock';
 import './stripesUtils.mock';
