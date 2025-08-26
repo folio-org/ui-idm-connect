a385f6f8dc97406205cc9a359eb26c644f1afbad
elsenhans <elsenhans@users.noreply.github.com>
2025-08-26 10:42:09 +0200

    UIIDM-62-use-functional-components settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch } from 'react-router-dom';
+import ReactRouterPropTypes from 'react-router-prop-types';
 
 import { Route } from '@folio/stripes/core';
 
@@ -13,34 +13,44 @@ import ChangeUBNumberRoute from './routes/ChangeUBNumberRoute';
 import ChangeUBNumberViewRoute from './routes/ChangeUBNumberViewRoute';
 import Settings from './settings';
 
-class IdmConnect extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-    showSettings: PropTypes.bool,
-  };
-
-  render() {
-    const { showSettings, match: { path } } = this.props;
-
-    if (showSettings) {
-      return <Settings {...this.props} />;
-    }
+const IdmConnect = ({
+  location,
+  match,
+  showSettings,
+  stripes,
+}) => {
+  if (showSettings) {
     return (
-      <Switch>
-        {/* <Route path={`${path}/change-ubnumber/view/:unilogin`} component={ChangeUBNumberViewRoute} />
-        <Route path={`${path}/change-ubnumber`} component={ChangeUBNumberRoute} /> */}
-        <Route path={`${path}/change-ubnumber/:unilogin?`} component={ChangeUBNumberRoute}>
-          <Route path={`${path}/change-ubnumber/view/:unilogin`} component={ChangeUBNumberViewRoute} />
-        </Route>
-        <Route path={`${path}/search-idm`} component={SearchIdmRoute} />
-        <Route path={`${path}/create`} component={ContractCreateRoute} />
-        <Route path={`${path}/:id/edit`} component={ContractEditRoute} />
-        <Route path={`${path}/:id?`} component={ContractsRoute}>
-          <Route path={`${path}/view/:id`} component={ContractViewRoute} />
-        </Route>
-      </Switch>
+      <Settings
+        location={location}
+        match={match}
+        stripes={stripes}
+      />
     );
   }
-}
+
+  return (
+    <Switch>
+      {/* <Route path={`${path}/change-ubnumber/view/:unilogin`} component={ChangeUBNumberViewRoute} />
+      <Route path={`${path}/change-ubnumber`} component={ChangeUBNumberRoute} /> */}
+      <Route path={`${match.path}/change-ubnumber/:unilogin?`} component={ChangeUBNumberRoute}>
+        <Route path={`${match.path}/change-ubnumber/view/:unilogin`} component={ChangeUBNumberViewRoute} />
+      </Route>
+      <Route path={`${match.path}/search-idm`} component={SearchIdmRoute} />
+      <Route path={`${match.path}/create`} component={ContractCreateRoute} />
+      <Route path={`${match.path}/:id/edit`} component={ContractEditRoute} />
+      <Route path={`${match.path}/:id?`} component={ContractsRoute}>
+        <Route path={`${match.path}/view/:id`} component={ContractViewRoute} />
+      </Route>
+    </Switch>
+  );
+};
+
+IdmConnect.propTypes = {
+  location: PropTypes.object.isRequired,
+  match: ReactRouterPropTypes.match.isRequired,
+  showSettings: PropTypes.bool,
+  stripes: PropTypes.object.isRequired,
+};
 
 export default IdmConnect;
--- src/settings/general-settings.js
+++ src/settings/general-settings.js
@@ -1,20 +1,20 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { Pane } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
 
-export default class GeneralSettings extends React.Component {
-  static propTypes = {
-    label: PropTypes.object.isRequired,
-  };
+import { Pane } from '@folio/stripes/components';
 
-  render() {
-    return (
-      <Pane defaultWidth="fill" fluidContentWidth paneTitle={this.props.label}>
-        <div>
-          <FormattedMessage id="ui-idm-connect.settings.general.message" />
-        </div>
-      </Pane>
-    );
-  }
-}
+const GeneralSettings = ({ label }) => {
+  return (
+    <Pane defaultWidth="fill" fluidContentWidth paneTitle={label}>
+      <div>
+        <FormattedMessage id="ui-idm-connect.settings.general.message" />
+      </div>
+    </Pane>
+  );
+};
+
+GeneralSettings.propTypes = {
+  label: PropTypes.object.isRequired,
+};
+
+export default GeneralSettings;
